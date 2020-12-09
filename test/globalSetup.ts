78b43cef315fe261bab4839441b26f60d2be53db
Dmitriy Pushkov <ezze@ezze.org>
2020-12-09 16:50:03 +0300

    Remove debug output.

--- test/globalSetup.ts
+++ test/globalSetup.ts
@@ -3,6 +3,5 @@ import fs from 'fs-extra';
 import { rootTestOutDirPath } from './helpers';
 
 export default async function setup(): Promise<void> {
-  console.log('====================================================');
   await fs.emptyDir(rootTestOutDirPath);
 }
